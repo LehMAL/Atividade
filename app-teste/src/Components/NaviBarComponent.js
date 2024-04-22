78a73dc1c5723681ebabbc0d48753299296498ff
Your Name <you@example.com>
2024-04-22 20:47:03 -0300

    novo

---
+++ app-teste/src/Components/NaviBarComponent.js
@@ -0,0 +1,37 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+
+function NavibarComponent() {
+
+  return (
+
+    <Navbar expand="sm" className="bg-body-tertiary">
+
+      <Container>
+
+        <Navbar.Brand href="/Page"></Navbar.Brand>
+
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        
+        <Navbar.Collapse id="basic-navbar-nav">
+
+          <Nav className="me-auto">
+            
+            <Nav.Link href="/home">Home</Nav.Link>
+            <Nav.Link href="/form">Formulário</Nav.Link>
+            <Nav.Link href="/us">Sobre Nós</Nav.Link>
+               
+          </Nav>
+    
+        </Navbar.Collapse>
+
+      </Container>
+
+    </Navbar>
+
+  );
+}
+
+export default NavibarComponent;
